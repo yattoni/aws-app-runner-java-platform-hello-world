ef746a24250b1c5eac9d1318d89175615b0d67bd
Austin Yattoni <austinyattoni@gmail.com>
2022-02-25 23:52:41 -0600

    Copy sample code from AWS docs.
    https://docs.aws.amazon.com/apprunner/latest/dg/service-source-code-java.html

---
+++ src/main/java/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index(){
+        String s = "Hello World";
+        return s;
+    }
+}
+
